4a99039041bd7528767e5b845c2fc380958338f9
Jamie <134149238+JCritch91@users.noreply.github.com>
2024-06-19 13:06:52 +0100

    create logic to loop 5 games create logic to declare an overall winner

--- java.js
+++ java.js
@@ -6,48 +6,58 @@ let humanSelection
 let compSelection
 
 
-
-
-function playRound(humanChoice, compChoice){
-console.log(humanChoice)
-console.log(compChoice)
-
-humanChoice == compChoice? console.log("It's a Draw"): 
-humanChoice == 'Rock' && compChoice == 'Paper'? console.log("You Lose"):
-humanChoice == 'Paper' && compChoice == 'Scissors'? console.log("You Lose"):
-humanChoice == 'Scissors' && compChoice == 'Rock'? console.log("You Lose"): console.log("You Win")
-}
-
-const compChoice = getCompChoice()
-const humanChoice = getPlayerChoice()
-
-
-
-function getPlayerChoice() {
-    let selection = prompt()
-    humanSelection = selection.substring(0,1).toUpperCase() + selection.substring(1).toLowerCase()
-    return
-}
-
-
-function getCompChoice(){
-    randomNumber = Math.floor(Math.random()*3)
-    console.log(randomNumber)
-
-    switch (randomNumber){
-        case 0:
-            compSelection = 'Rock'
-        break;
+function playGame(){
+    for (let i =0; i<5; i++){
     
-        case 1:
-            compSelection = 'Paper'
-        break;
-    
-        case 2:
-            compSelection = 'Scissors'
-        break;
+        function playRound(humanChoice, compChoice){
+        console.log(humanChoice)
+        console.log(compChoice)
+
+        humanChoice == compChoice? console.log("It's a Draw"): 
+        humanChoice == 'Rock' && compChoice == 'Paper'? (computerScore ++, console.log("You Lose")):
+        humanChoice == 'Paper' && compChoice == 'Scissors'? (computerScore ++, console.log("You Lose")):
+        humanChoice == 'Scissors' && compChoice == 'Rock'? (computerScore ++, console.log("You Lose")): (humanScore ++, console.log("You Win"))
+
+        console.log("Human Score = " + humanScore)
+        console.log("Computer Score = " + computerScore)
+        }
+
+        const compChoice = getCompChoice()
+        const humanChoice = getPlayerChoice()
+
+
+
+        function getPlayerChoice() {
+            let selection = prompt()
+            humanSelection = selection.substring(0,1).toUpperCase() + selection.substring(1).toLowerCase()
+            return
+        }
+
+        function getCompChoice(){
+            randomNumber = Math.floor(Math.random()*3)
+
+            switch (randomNumber){
+                case 0:
+                    compSelection = 'Rock'
+                break;
+            
+                case 1:
+                    compSelection = 'Paper'
+                break;
+            
+                case 2:
+                    compSelection = 'Scissors'
+                break;
+            }
+            return 
+        }
+
+        playRound(humanSelection, compSelection)
     }
-    return 
+    computerScore>humanScore? console.log("Game Over. Compter Wins " + computerScore + "-" + humanScore):
+    humanScore>computerScore? console.log("Game Over You Win! " + humanScore + "-" + computerScore):
+    console.log("Game Over. It's a Draw " + humanScore + "-" + computerScore)
 }
 
-playRound(humanSelection, compSelection)
+
+playGame()
