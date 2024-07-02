e0d44519ddbe6a7fb76ce2e925d1d36c09d7812d
Jamie <134149238+JCritch91@users.noreply.github.com>
2024-07-02 12:26:14 +0100

    ADDED STYLING ADDED LOGIC FOR RESETTING

--- java.js
+++ java.js
@@ -1,17 +1,48 @@
-const rockButton = document.querySelector(".rock")
-const paperButton = document.querySelector(".paper")
-const scissorsButton = document.querySelector(".scissors")
+const buttons = document.querySelector(".buttons")
 const selectionDisplay = document.querySelector(".selectionDisplay")
+const humanCount = document.querySelector(".humanCount")
+const compCount = document.querySelector(".compCount")
 const roundResultDisplay = document.querySelector(".roundResultDisplay")
-const roundScoreDisplay = document.querySelector(".roundScoreDisplay")
-const overallResultDisplay = document.querySelector(".overallResultDisplay")
+
+const rockButton = document.createElement("button")
+rockButton.classList.add("rock")
+rockButton.addEventListener('click', () => {
+    if (humanScore + computerScore < 5){
+    playGame('Rock')
+    }
+    return
+})
+
+const paperButton = document.createElement("button")
+paperButton.classList.add("paper")
+paperButton.addEventListener('click', () => {
+    if (humanScore + computerScore < 5){
+    playGame('Paper')
+    }
+    return
+})
+
+const scissorsButton = document.createElement("button")
+scissorsButton.classList.add("scissors")
+scissorsButton.addEventListener('click', () => {
+    if (humanScore + computerScore < 5){
+    playGame('Scissors')
+    }
+    return
+})
+
+buttons.appendChild(rockButton)
+buttons.appendChild(paperButton)
+buttons.appendChild(scissorsButton)
+
+const resetButton = document.createElement("button")
+resetButton.textContent = "Play Again"
+resetButton.classList.add("reset")
+resetButton.addEventListener('click', reset)
 
 let humanScore = 0
 let computerScore = 0
-/* let humanSelection */
 let compSelection
-
-
 
 
 
@@ -19,26 +50,16 @@ function playGame(humanChoice){
 
         function playRound(humanChoice, compChoice){
 
-        selectionDisplay.textContent = "You Chose " + humanChoice + ", Computer Chose " + compChoice
-
-        humanChoice == compChoice? roundResultDisplay.textContent = "It's a Draw": 
-        humanChoice == 'Rock' && compChoice == 'Paper'? (computerScore ++, roundResultDisplay.textContent = "You Lose"):
-        humanChoice == 'Paper' && compChoice == 'Scissors'? (computerScore ++, roundResultDisplay.textContent = "You Lose"):
-        humanChoice == 'Scissors' && compChoice == 'Rock'? (computerScore ++, roundResultDisplay.textContent = "You Lose"): (humanScore ++, roundResultDisplay.textContent = "You Win")
-
-        roundScoreDisplay.textContent = "Human Score = " + humanScore +" , Computer Score = " + computerScore
+        roundResultDisplay.textContent = "You Chose " + humanChoice + ", Computer Chose " + compChoice
+ 
+        humanChoice == 'Rock' && compChoice == 'Paper'? computerScore ++:
+        humanChoice == 'Paper' && compChoice == 'Scissors'? computerScore ++:
+        humanChoice == 'Scissors' && compChoice == 'Rock'? computerScore ++: humanScore ++
+        humanCount.textContent = humanScore 
+        compCount.textContent = computerScore
         }
 
         const compChoice = getCompChoice()
-/*         const humanChoice = getPlayerChoice() */
-
-
-
-/*         function getPlayerChoice() {
-            let selection = prompt()
-            humanSelection = selection.substring(0,1).toUpperCase() + selection.substring(1).toLowerCase()
-            return
-        } */
 
         function getCompChoice(){
             randomNumber = Math.floor(Math.random()*3)
@@ -63,30 +84,28 @@ function playGame(humanChoice){
 
         if (humanScore + computerScore == 5){
             endResult()
+            return
         }
     
 
 }
 
 function endResult(){
-    computerScore>humanScore? overallResultDisplay.textContent = "Game Over. Compter Wins " + computerScore + "-" + humanScore:
-    humanScore>computerScore? overallResultDisplay.textContent =  "Game Over You Win! " + humanScore + "-" + computerScore:
-    overallResultDisplay.textContent = "Game Over. It's a Draw " + humanScore + "-" + computerScore
+    buttons.replaceChildren()
+    computerScore>humanScore? roundResultDisplay.textContent = "Game Over. You Lose..":
+    humanScore>computerScore? roundResultDisplay.textContent =  "Game Over You Win!":
+    roundResultDisplay.textContent = "Game Over. It's a Draw"
+    buttons.appendChild(resetButton)
 }
 
-
-
-rockButton.addEventListener('click', () => {
-    playGame('Rock')
-    return
-})
-
-paperButton.addEventListener('click', () => {
-    playGame('Paper')
-    return
-})
-
-scissorsButton.addEventListener('click', () => {
-    playGame('Scissors')
-    return
-})
+function reset(){
+roundResultDisplay.textContent =''
+humanCount.textContent ='0'
+compCount.textContent ='0'
+humanScore = 0
+computerScore = 0
+buttons.removeChild(resetButton)
+buttons.appendChild(rockButton)
+buttons.appendChild(paperButton)
+buttons.appendChild(scissorsButton)
+}
